0ac0c6839e14554c7f607d8dfe12918cddeb6b55
coderazzi <coderazzi@gmail.com>
2012-12-06 00:18:14 +0100

    Improved the example to avoid using remote files for images on html countries, it could slow down enormously the application. It still works fine when executed locally

--- examples/src/main/java/net/coderazzi/filters/examples/utils/TestData.java
+++ examples/src/main/java/net/coderazzi/filters/examples/utils/TestData.java
@@ -24,8 +24,12 @@
  */
 package net.coderazzi.filters.examples.utils;
 
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -36,6 +40,7 @@ import java.util.regex.Pattern;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
@@ -137,10 +142,19 @@ public class TestData {
         private static final long serialVersionUID = 1242769439980562528L;
         private Double redAmount;
         private String fileLocation;
+        private static boolean webStart;
+        
+        static {
+        	try{
+        		Class.forName("javax.jnlp.ServiceManager");
+        		webStart = true;
+        	} catch(Exception ex){
+        		webStart = false;
+        	}
+        }
 
-        Flag(String name, byte array[]) {
+        Flag(byte array[]) {
             super(array);
-            fileLocation = "http://coderazzi.net/private/flags/"+name;
         }
 
         public double getRedAmount() {
@@ -173,8 +187,24 @@ public class TestData {
 
             return redAmount;
         }
-
+        
         public String getFileLocation(){
+        	if (fileLocation==null && !webStart){
+        		Image img = getImage();
+        		BufferedImage bi = new BufferedImage(img.getWidth(null),
+        				img.getHeight(null),
+        				BufferedImage.TYPE_4BYTE_ABGR);
+        		Graphics2D g2 = bi.createGraphics();
+        		g2.drawImage(img, 0, 0, null);
+        		g2.dispose();
+        		try{
+        			File temp = File.createTempFile("tablefilter", ".jpg"); 
+        			ImageIO.write(bi, "jpg", temp);
+        			fileLocation = "file://"+temp.getAbsolutePath();
+        		} catch(IOException ex){
+        			fileLocation="";
+        		}
+        	}
         	return fileLocation;
         }
     }
@@ -221,7 +251,7 @@ public class TestData {
         // date is not exact (not everybody can be born at 00:00!)
         date = new GregorianCalendar(random.nextInt(50) + 1940,
                 random.nextInt(12), random.nextInt(28), 1, 1).getTime();
-                
+        
         if (random.nextBoolean() && random.nextBoolean()) {
         	if (random.nextBoolean()) {
         		note = "<html><i>Transferral <font color='red'>"+
@@ -262,7 +292,7 @@ public class TestData {
                         baos.write(buffer, 0, read);
                     }
 
-                    Flag ic = new Flag(entry.getName(), baos.toByteArray());
+                    Flag ic = new Flag(baos.toByteArray());
                     ic.setDescription(m.group(1));
                     allIcons.add(ic);
                 }
