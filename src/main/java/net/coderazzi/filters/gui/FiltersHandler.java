e9732e77cf3a74799f9aef50164aa367d7e32cbc
coderazzi <coderazzi@gmail.com>
2013-03-13 00:36:09 +0100

    Backed out changeset b16a24059010 discarded changes

---
+++ src/main/java/net/coderazzi/filters/gui/FiltersHandler.java
@@ -0,0 +1,381 @@
+/**
+ * Author:  Luis M Pena  ( lu@coderazzi.net )
+ * License: MIT License
+ *
+ * Copyright (c) 2007 Luis M. Pena  -  lu@coderazzi.net
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package net.coderazzi.filters.gui;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.JTable;
+import javax.swing.table.TableModel;
+
+import net.coderazzi.filters.AndFilter;
+import net.coderazzi.filters.ComposedFilter;
+import net.coderazzi.filters.Filter;
+import net.coderazzi.filters.IFilter;
+import net.coderazzi.filters.IFilterObserver;
+import net.coderazzi.filters.artifacts.ITableModelFilter;
+import net.coderazzi.filters.artifacts.RowFilter;
+import net.coderazzi.filters.artifacts.TableModelFilter;
+import net.coderazzi.filters.gui.editor.FilterEditor;
+
+
+/**
+ * <p>FiltersHandler represents a {@link RowFilter} instance that can be
+ * attached to a {@link javax.swing.JTable} to compose dynamically the outcome
+ * of one or more filter editors. As such, it is a dynamic filter, which updates
+ * the table when there are changes in any of the composed sub filters.</p>
+ *
+ * <p>Users have, after version 3.2, no direct use for this class</p>
+ *
+ * <p>In Java 5, the {@link javax.swing.JTable} has no sorting or filtering
+ * capabilities, so the implementation of the TableFilter is slightly different
+ * in Java 5 and 6. When the table is attached, it should contain already the
+ * model to filter, and the model itself should be an instance of {@link
+ * ITableModelFilter}. If this is not the case, the TableFilter automatically
+ * creates a (@link ITableModelFilter} and attaches it to the table.</p>
+ *
+ * @author  Luis M Pena - lu@coderazzi.net
+ */
+public class FiltersHandler extends AndFilter
+    implements PropertyChangeListener {
+
+    /**
+     * sendNotifications is used internally as a semaphore to disable
+     * temporarily notifications to the filter observers. Notifications are only
+     * sent to the observers when this variable is non negative.
+     */
+    int sendNotifications = 0;
+
+    /**
+     * pendingNotifications keeps track of notifications to be sent to the
+     * observers, but were discarded because the variable sendNotifications was
+     * negative.
+     */
+    private boolean pendingNotifications;
+
+    /** The autoChoices mode.*/
+    private AutoChoices autoChoices;
+
+    /** All the editors, mapped by their filter position. */
+    private Map<Integer, FilterEditor> editors =
+        new HashMap<Integer, FilterEditor>();
+
+    /** The associated table. */
+    private JTable table;
+
+    /** Instance to handle choices (choices) on each FilterEditor. */
+    private ChoicesHandler choicesHandler = new NonAdaptiveChoicesHandler(this);
+
+    /** The associated filter model. */
+    private IParserModel parserModel;
+
+    /** The associated filter model. */
+    private Filter applyingFilter;
+
+    /** Only constructor. */
+    FiltersHandler(AutoChoices mode, IParserModel parserModel) {
+    	
+    	autoChoices = mode;
+    	// create an observer instance to notify the associated table when there
+        // are filter changes.
+        addFilterObserver(new IFilterObserver() {
+                public void filterUpdated(IFilter obs) {
+                    notifyUpdatedFilter();
+                }
+            });
+        setParserModel(parserModel);
+    }
+
+    /**
+     * Method to set the associated table. 
+     */
+    public void setTable(JTable table) {
+        choicesHandler.setInterrupted(true);
+        if (this.table != null) {
+            TableModel tm = this.table.getModel();
+            if (tm instanceof ITableModelFilter) {
+                this.table.setModel(((ITableModelFilter) tm).getModel());
+            }
+        }
+
+        this.table = table;
+    }
+
+    /** Returns the associated table. */
+    public JTable getTable() {
+        return table;
+    }
+
+    /** Sets the {@link IParserModel} instance. */
+    public void setParserModel(IParserModel parserModel) {
+        if ((parserModel != null) && (parserModel != this.parserModel)) {
+            if (this.parserModel != null) {
+                this.parserModel.removePropertyChangeListener(this);
+            }
+
+            this.parserModel = parserModel;
+            this.parserModel.addPropertyChangeListener(this);
+            enableNotifications(false);
+            for (FilterEditor editor : editors.values()) {
+                editor.resetFilter();
+            }
+
+            enableNotifications(true);
+        }
+
+        this.parserModel = parserModel;
+    }
+
+    /** Returns the registered {@link IParserModel} instance. */
+    public IParserModel getParserModel() {
+        return parserModel;
+    }
+
+    /**
+     * {@link PropertyChangeListener} interface, for changes on {@link
+     * IParserModel}.
+     */
+    public void propertyChange(PropertyChangeEvent evt) {
+        Class target;
+        boolean formatChange = false;
+        if (IParserModel.IGNORE_CASE_PROPERTY.equals(evt.getPropertyName())) {
+            target = null;
+        } else {
+            if (IParserModel.FORMAT_PROPERTY.equals(evt.getPropertyName())) {
+                formatChange = true;
+            } else if (
+                !IParserModel.COMPARATOR_PROPERTY.equals(
+                        evt.getPropertyName())) {
+                return;
+            }
+
+            // in this case, the target is the affected class
+            Object cl = evt.getNewValue();
+            if (cl instanceof Class) {
+                target = (Class) cl;
+            } else {
+                return;
+            }
+        }
+
+        enableNotifications(false);
+        for (FilterEditor editor : editors.values()) {
+            if (target == null) {
+                editor.setIgnoreCase(parserModel.isIgnoreCase());
+            } else if (editor.getModelClass() == target) {
+                if (formatChange) {
+                    editor.setFormat(parserModel.getFormat(target));
+                } else {
+                    editor.setComparator(parserModel.getComparator(target));
+                }
+            }
+        }
+
+        enableNotifications(true);
+    }
+
+    @Override public void setEnabled(boolean enabled) {
+        enableNotifications(false);
+        super.setEnabled(enabled);
+        enableNotifications(true);
+    }
+
+    /** Sets/unsets the auto choices flag. */
+    public void setAutoChoices(AutoChoices mode) {
+        if (mode != autoChoices) {
+            enableNotifications(false);
+            this.autoChoices = mode;
+            for (FilterEditor editor : editors.values()) {
+                // after this call, the editor will request its choices
+                editor.setAutoChoices(mode);
+            }
+
+            enableNotifications(true);
+        }
+    }
+
+    /** Returns the auto choices mode. */
+    public AutoChoices getAutoChoices() {
+        return autoChoices;
+    }
+
+    /** {@link ComposedFilter} interface. */
+    @Override public void addFilter(IFilter... filtersToAdd) {
+        choicesHandler.filterOperation(true);
+        super.addFilter(filtersToAdd);
+        choicesHandler.filterOperation(false);
+    }
+
+    /** {@link ComposedFilter} interface. */
+    @Override public void removeFilter(IFilter... filtersToRemove) {
+        choicesHandler.filterOperation(true);
+        super.removeFilter(filtersToRemove);
+        choicesHandler.filterOperation(false);
+    }
+
+    /** Adds a new filter editor, called from the {@link TableFilterHeader}. */
+    public void addFilterEditor(FilterEditor editor) {
+        super.addFilter(editor.getFilter());
+        editors.put(editor.getModelIndex(), editor);
+        editor.setAutoChoices(autoChoices);
+    }
+
+    /** Removes a filter editor, called from the {@link TableFilterHeader}. */
+    public void removeFilterEditor(FilterEditor editor) {
+        super.removeFilter(editor.getFilter());
+        editors.remove(editor.getModelIndex());
+    }
+
+    /**
+     * Method invoked by the FilterEditor when its autoChoices mode OR user
+     * choices change; in return, it will set the proper choices on the
+     * specified editor.
+     */
+    public void updateEditorChoices(FilterEditor editor) {
+        if (editors.containsValue(editor) && isEnabled()) {
+            choicesHandler.editorUpdated(editor);
+        }
+    }
+
+    /** {@link ComposedFilter} interface. */
+    @Override public void filterUpdated(IFilter filter) {
+        boolean wasEnabled = isEnabled();
+        boolean filterWasDisabled = isDisabled(filter);
+        if (filter != applyingFilter) {
+            choicesHandler.filterUpdated(filter, false);
+        }
+
+        super.filterUpdated(filter);
+        if (filterWasDisabled && filter.isEnabled()) {
+            choicesHandler.filterEnabled(filter);
+        } else if (wasEnabled && !isEnabled()) {
+            choicesHandler.allFiltersDisabled();
+        }
+    }
+
+    /**
+     * Applies the passed filter, from an associated editor, and, on success,
+     * reports it to observers. This method can be called -instead of the usual
+     * FilterEditor.reportFilterUpdatedToObservers to detect if the new filter
+     * will filter out all the rows.
+     */
+    public boolean applyEditorFilter(Filter filter) {
+        boolean ret = choicesHandler.filterUpdated(filter, true);
+        if (ret) {
+            applyingFilter = filter;
+            filter.reportFilterUpdatedToObservers();
+            applyingFilter = null;
+        }
+
+        return ret;
+    }
+
+    /** Internal method to set/update the filtering. */
+    public void updateTableFilter() {
+        pendingNotifications = false;
+        if (table != null) {
+            TableModel model = table.getModel();
+            if (model != null) {
+                getRowFilter(model).setRowFilter(isEnabled() ? this : null);
+            }
+        }
+    }
+
+    /** Returns all registered {@link FilterEditor}s. */
+    public Collection<FilterEditor> getEditors() {
+        return editors.values();
+    }
+
+    /** Returns the {@link FilterEditor} instance on the given column. */
+    public FilterEditor getEditor(int column) {
+        return editors.get(column);
+    }
+
+    /**
+     * <p>Temporarily enable/disable notifications to the observers, including
+     * the registered {@link javax.swing.JTable}.</p>
+     *
+     * <p>Multiple calls to this method can be issued, but the caller must
+     * ensure that there are as many calls with true parameter as with false
+     * parameter, as the notifications are only re-enabled when the zero balance
+     * is reached.</p>
+     */
+    public void enableNotifications(boolean enable) {
+        sendNotifications += enable ? 1 : -1;
+        if (enable) {
+            if (sendNotifications == 0) {
+
+                // adding/removing filter editors is not done as separate
+                // processes when the user sets/changes the TableModel, all
+                // columns are removed and then recreated. At the beginning of
+                // the process, there are calls to
+                // enableNotifications(false)/(true) than only balance out when
+                // the whole model is setup. We use the same mechanism whenever
+                // it would be needed to recreate the adaptive support or
+                // because it could be more efficient doing so.
+                if (choicesHandler.setInterrupted(false)
+                        || pendingNotifications) {
+                    updateTableFilter();
+                }
+            }
+        } else if (choicesHandler.setInterrupted(true)) {
+            // updateTableFilter();
+        }
+    }
+
+    /**
+     * Internal method to send a notification to the observers, verifying first
+     * if the notifications are currently enabled.
+     */
+    void notifyUpdatedFilter() {
+        if (sendNotifications < 0) {
+            pendingNotifications = true;
+        } else {
+            updateTableFilter();
+        }
+    }
+
+
+    /**
+     * Returns the row filter associated to the current table, creating a
+     * default one if none.
+     */
+    private ITableModelFilter getRowFilter(TableModel tableModel) {
+        if (tableModel instanceof ITableModelFilter) {
+            return (TableModelFilter) tableModel;
+        }
+
+        TableModelFilter modelFilter = new TableModelFilter(tableModel);
+        table.setModel(modelFilter);
+
+        return modelFilter;
+    }
+
+}
---
+++ src/main/java/net/coderazzi/filters/gui/editor/ChoicesListModel.java
@@ -0,0 +1,284 @@
+/**
+ * Author:  Luis M Pena  ( lu@coderazzi.net )
+ * License: MIT License
+ *
+ * Copyright (c) 2007 Luis M. Pena  -  lu@coderazzi.net
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package net.coderazzi.filters.gui.editor;
+
+import java.text.Format;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import javax.swing.AbstractListModel;
+
+import net.coderazzi.filters.gui.CustomChoice;
+
+
+abstract class ChoicesListModel extends AbstractListModel {
+
+    private static final long serialVersionUID = 2233861800925325272L;
+    private Format format;
+    Comparator choicesComparator;
+    Comparator stringComparator;
+    
+    public static ChoicesListModel getCustom(ChoicesListModel current, 
+    		Format format, boolean renderedContent,
+    		Comparator choicesComparator, Comparator stringComparator){
+    	if (current!=null 
+    			&& (renderedContent == (current instanceof RenderedChoicesListModel))
+    			&& current.matches(format, choicesComparator, stringComparator)){
+    		return current;
+    	}
+    	if (renderedContent){
+    		return new RenderedChoicesListModel(choicesComparator, stringComparator);
+    	}
+    	if (stringComparator.equals(choicesComparator)){
+    		return new StringChoicesListModel(format, stringComparator);
+    	}
+    	return new GenericChoicesListModel(format, choicesComparator, stringComparator);
+    }
+
+    public ChoicesListModel(Format format, Comparator choicesComparator, Comparator stringComparator) {
+        this.format = format;
+        this.choicesComparator = choicesComparator;
+        this.stringComparator = stringComparator;
+    }
+
+    /** Clears all content (but ALL matcher). */
+    public abstract void clearContent();
+
+    public boolean matches(Format format, Comparator choicesComparator, Comparator stringComparator){
+    	if (this.choicesComparator.equals(choicesComparator) && this.stringComparator.equals(stringComparator)){
+    		return this.format==null? format==null : this.format.equals(format);
+    	}
+    	return false;
+    }
+
+    /**
+     * Adds additional choices.<br>
+     * If the content is text-based, the choices are converted into Strings, and
+     * sorted; additionally, choices are also escaped.<br>
+     * Otherwise, no sorting is performed, although duplicates are still
+     * discarded
+     *
+     * @return  true if there are any changes after the operation
+     */
+    public boolean addContent(Collection addedContent, IChoicesParser parser) {
+        boolean changed = false;
+        for (Object o : addedContent) {
+            String s = null;
+            if (!(o instanceof CustomChoice)) {
+                if (o == null) {
+                    o = CustomChoice.MATCH_EMPTY;
+                } else {
+                    s = (format == null) ? o.toString() : format.format(o);
+                    if (s.length() == 0) {
+                        o = CustomChoice.MATCH_EMPTY;
+                    } else {
+                        s = parser.escapeChoice(s);
+                    }
+                }
+            }
+            changed = addContent(o, s) || changed;
+        }
+
+        if (changed) {
+            fireContentsChanged(this, 0, getSize());
+        }
+
+        return changed;
+    }
+
+
+    /** @see  PopupComponent#selectBestMatch(Object, boolean) */
+    public abstract ChoiceMatch getClosestMatch(Object hint, boolean exact);
+
+    /**
+     * Returns the text that could complete the given string<br>
+     * The completion string is the larger string that matches all existing
+     * options in this model or the added list.
+     */
+    public abstract String getCompletion(String base, List addedList);
+
+    /**
+     * Method invoked by addContent.
+     *
+     * @param   content
+     * @param   repr
+     *
+     * @return
+     */
+    protected boolean addContent(Object content, String repr) {
+        return false;
+    }
+
+    /**
+     * Returns the text that could complete the given string<br>
+     * The completion string is the larger string that matches all existing
+     * options in this model or the added list.
+     */
+    protected String getCompletion(List       sortedContent,
+                                   Object     baseAsContent,
+                                   Comparator sortingComparator,
+                                   List       unsortedContent,
+                                   List       additionalUnsortedContent) {
+        int pos = Collections.binarySearch(sortedContent, baseAsContent,
+                sortingComparator);
+        if (pos >= 0) {
+            // exact match, do nothing else
+            return "";
+        }
+
+        String base = baseAsContent.toString();
+        String ret = base;
+        int len = ret.length();
+        int cs = sortedContent.size();
+        pos = -pos - 1;
+        if (pos < cs) {
+            String use = sortedContent.get(pos).toString();
+            // the position found should start with the base string.
+            // if not, no choice start with it
+            if (ChoiceMatch.getMatchingLength(base, use, stringComparator)
+                    >= len) {
+                ret = use;
+
+                int maxLen = ret.length();
+                while (++pos < cs) {
+                    use = sortedContent.get(pos).toString();
+
+                    int m = ChoiceMatch.getMatchingLength(ret, use,
+                            stringComparator);
+                    if (m < len) {
+                        // options are sorted, as soon as one does not start
+                        // with the base string, no one else will
+                        break;
+                    } else if (m == len) {
+                        return "";
+                    } else if (m < maxLen) {
+                        maxLen = m;
+                    }
+                }
+
+                ret = ret.substring(0, maxLen);
+            }
+        }
+
+        for (int i = 1; i <= 2; i++) {
+            List l = (i == 1) ? unsortedContent : additionalUnsortedContent;
+            if (l != null) {
+                for (Object o : l) {
+                    String s = o.toString();
+                    int m = ChoiceMatch.getMatchingLength(ret, s,
+                            stringComparator);
+                    if (m == len) {
+                        if (ret != base) {
+                            return ""; // exact match!
+                        }
+
+                        ret = s;
+                    } else if (m > len) {
+                        ret = ret.substring(0, m);
+                    }
+                }
+            }
+        }
+
+        return ret.substring(len);
+    }
+
+    /** Creation of the Match, for text based, sorted content. */
+    protected <T> T findOnSortedContent(ChoiceMatch base,
+                                        List<T>     sortedContent,
+                                        Comparator  sortingComparator,
+                                        T           hint,
+                                        boolean     fullMatch) {
+        T found = null;
+        int pos = Collections.binarySearch(sortedContent, hint,
+                sortingComparator);
+        if (pos >= 0) {
+            // found it, do nothing else (it is exact)
+            base.exact = true;
+            base.index = pos;
+            found = sortedContent.get(pos);
+        } else if (!fullMatch) {
+            String strStart = hint.toString();
+            // try the two positions around
+            int suggested = -pos - 1;
+            if (suggested < sortedContent.size()) {
+                T suggestion = sortedContent.get(suggested);
+                int len = ChoiceMatch.getMatchingLength(strStart,
+                        suggestion.toString(), stringComparator);
+                if ((len > base.len) || (base.len == 0)) {
+                    base.index = suggested;
+                    found = suggestion;
+                    base.len = len;
+                }
+            }
+            // if suggested is in the custom choices, no need to try
+            if (--suggested >= 0) {
+                T suggestion = sortedContent.get(suggested);
+                int len = ChoiceMatch.getMatchingLength(strStart,
+                        suggestion.toString(), stringComparator);
+                if ((len > base.len) || (base.len == 0)) {
+                    base.index = suggested;
+                    base.len = len;
+                    found = suggestion;
+                }
+            }
+        }
+
+        return found;
+    }
+
+
+    /**
+     * Comparator for collections containing {@link CustomChoice} instances:
+     * those instances are sorted by their own precedence definitions, and have
+     * precedence over the rest of instances in the collection
+     */
+    class CustomChoiceAwareComparator implements Comparator<Object> {
+
+        public int compare(Object o1, Object o2) {
+            if (o1 instanceof CustomChoice) {
+                if (o2 instanceof CustomChoice) {
+                    CustomChoice c1 = (CustomChoice) o1;
+                    CustomChoice c2 = (CustomChoice) o2;
+                    int ret = c1.getPrecedence() - c2.getPrecedence();
+                    if (ret == 0) {
+                        ret = stringComparator.compare(c1.toString(),
+                                c2.toString());
+                    }
+
+                    return ret;
+                }
+
+                return -1;
+            } else if (o2 instanceof CustomChoice) {
+                return 1;
+            }
+            return choicesComparator.compare(o1, o2);
+        }
+    }
+}
